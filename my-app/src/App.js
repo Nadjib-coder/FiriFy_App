7c04ad9c269568fc4830562f96186a51f5b2666c
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-10-05 21:26:59 +0100

    install react bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>FiriFy</h1>
+        <h1>FiriFy - Welcom to our app</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
