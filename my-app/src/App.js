8bb07aabd3a0134566f24f9b696aac83997c43f1
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-10-06 02:34:37 +0100

    complet section 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/Navigation';
+import Section1 from './components/Section1';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Section1 />
     </div>
   );
 }
